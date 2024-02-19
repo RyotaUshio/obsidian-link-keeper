a752b4a0147866c757716462617dba5cadd0a5e2
RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
2024-02-19 16:39:44 +0900

    release: 0.1.0

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,68 @@
-import { Plugin } from 'obsidian';
-import { MyPluginSettings, DEFAULT_SETTINGS, SampleSettingTab } from 'settings';
+import { Plugin, TFile, TFolder, normalizePath } from 'obsidian';
 
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class LinkKeeperPlugin extends Plugin {
+	settings: LinkKeeperSettings;
+
+	activeTask: Promise<any> = Promise.resolve();
+	failedTasks: Promise<any>[] = [];
 
 	async onload() {
-		await this.loadSettings();
-		await this.saveSettings();
-		this.addSettingTab(new SampleSettingTab(this));
-	}
+		this.registerEvent(this.app.vault.on('create', (newFile) => {
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+			const eventRef = this.app.vault.on('delete', async (oldFile) => {
 
-	async saveSettings() {
-		await this.saveData(this.settings);
+				if ((oldFile instanceof TFile && newFile instanceof TFile)
+					|| (oldFile instanceof TFolder && newFile instanceof TFolder)) {
+
+					const oldPath = oldFile.path;
+					const newPath = newFile.path;
+
+					const oldParent = normalizePath(oldPath.split('/').slice(0, -1).join('/')); // TFile.parent is null for a deleted file
+					const newParent = newFile.parent.path;
+
+					if (oldFile.name === newFile.name // moved to a different folder
+						|| oldParent === newParent) { // renamed in the same folder
+
+						const newPath = newFile.path;
+
+						const previousTask = this.activeTask;
+
+						const task = async () => {
+							await this.app.vault.rename(newFile, oldFile.path);
+							await this.app.fileManager.renameFile(newFile, newPath);
+						};
+
+						this.activeTask = (async () => {
+							await previousTask;
+							try {
+								await task();
+
+								this.app.vault.offref(eventRef);
+
+								const retryFailedTasks = [];
+
+								while (this.failedTasks.length) {
+									const failedTask = this.failedTasks.shift();
+									try {
+										await failedTask();
+									} catch (e) {
+										retryFailedTasks.push(failedTask);
+									}
+								}
+
+								this.failedTasks.push(...retryFailedTasks);
+							} catch (e) {
+								this.failedTasks.push(task);
+							}
+						})();
+					}
+				}
+			});
+
+			window.setTimeout(() => {
+				this.app.vault.offref(eventRef);
+			}, 500);
+		}))
 	}
 }
